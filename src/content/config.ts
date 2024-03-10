7a206cd937157f7ad9f29b2065223261e44685a9
Xevion <xevion@xevion.dev>
2024-03-10 07:44:20 -0500

    Continue switching to handbook content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const handbook = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
@@ -8,9 +8,9 @@ const blog = defineCollection({
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+		lastModified: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { handbook };
