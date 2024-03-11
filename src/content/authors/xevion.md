85271109f86d70927e69223fa7dbaf65af0c0808
Xevion <xevion@xevion.dev>
2024-03-10 23:47:23 -0500

    Add author data collection

---
+++ src/content/authors/xevion.md
@@ -0,0 +1,7 @@
+---
+username: "xevion"
+displayName: "Ryan Walters"
+discord: ".xevion"
+github: "xevion"
+email: "ryan@walters.to"
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const handbook = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		lastModified: z.coerce.date().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    lastModified: z.coerce.date().optional(),
+  }),
+});
+const authors = defineCollection({
+  type: "data",
+  schema: z.object({
+    username: z.string(),
+    displayName: z.string(),
+    email: z.string().optional(),
+    discord: z.string().optional(),
+    github: z.string().optional(),
+  }),
 });
 
-export const collections = { handbook };
+export const collections = { authors, handbook };
